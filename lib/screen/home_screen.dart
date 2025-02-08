88daa6812cae928bffa0cfec3a5957b4f08b70ce
sshinn0224 <sshinn0224@gmail.com>
2025-02-08 10:25:04 +0900

    feature : 스케쥴 카드 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,21 @@
 import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key : key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key:key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +23,28 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
           child: Column(
             children: [
-              MainCalendar()
+              MainCalendar(
+                selectedDate: selectedDate,
+                onDaySelected: onDaySelected,
+              ),
+              SizedBox(height: 8.0),
+              TodayBanner(
+                  selectedDate: selectedDate,
+                  count: 0),
+              SizedBox(height: 8.0),
+              ScheduleCard(
+                  startTime: 12,
+                  endTime: 14,
+                  content: '프로그래밍 공부')
             ],
           )
       )
     );
-  }}
+
+  }
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
